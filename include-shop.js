ff5f785c029a626993c843b997b9f079cfffb854
10tenyearsoneday-glitch <10tenyears.oneday@gmail.com>
2026-01-08 23:01:53 +0800

    Create include-shop.js

---
+++ include-shop.js
@@ -0,0 +1,8 @@
+<!-- index.html / all.html / product.html -->
+<script src="./include-shop.js"></script>
+(() => {
+  if (window.TEN_SHOP_LOADED) return;
+  window.TEN_SHOP_LOADED = true;
+
+  // 這裡面「完全不用考慮 member」
+})();
