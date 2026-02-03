b0e9b27eb11bd43954141d293ce48d74452468e3
10tenyearsoneday-glitch <10tenyears.oneday@gmail.com>
2026-02-03 15:29:57 +0800

    Update include-member.js

--- include-member.js
+++ include-member.js
@@ -260,7 +260,7 @@ if(!address) return toast($("regToast"),"請填地址");
 ========================= */
 document.addEventListener("DOMContentLoaded", () => {
   const token = localStorage.getItem("ten_token");
-  if (!token) return;
+  if (!token) return};
 
   // 讀取會員資料
 window.__meCb = res => {
@@ -300,7 +300,7 @@ if(saveBtn){
 
       window.__profileSaveCb = r => {
         document.getElementById("profileToast").textContent =
-          r.ok ? "已儲存" : "儲存失敗";
+          r.ok ? "已儲存" : "儲存失敗"};
       };
 
       const qs =
